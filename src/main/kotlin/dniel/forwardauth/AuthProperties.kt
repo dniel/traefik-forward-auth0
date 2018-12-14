3b1a2fcb3ef9a43f9cb9b008371fe52df35a5a32
Daniel <daniel@engfeldt.net>
2018-12-14 21:42:28 +0100

    Add validation annotations to application configuration class to validate that all fields in the config has been filled out.

--- src/main/kotlin/dniel/forwardauth/AuthProperties.kt
+++ src/main/kotlin/dniel/forwardauth/AuthProperties.kt
@@ -1,25 +1,47 @@
 package dniel.forwardauth
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import javax.validation.constraints.NotBlank
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
+@Validated
 @ConfigurationProperties()
 class AuthProperties {
+    @NotEmpty
     lateinit var domain: String
+
+    @NotEmpty
     lateinit var tokenEndpoint: String
+
+    @NotEmpty
     lateinit var authorizeUrl: String
 
+    @NotNull
     val default = Application()
+
     val apps = ArrayList<Application>()
 
     class Application {
+        @NotEmpty
         lateinit var name: String
+
+        @NotEmpty
         lateinit var clientId: String
+
+        @NotEmpty
         lateinit var clientSecret: String
+
+        @NotEmpty
         lateinit var audience: String
 
+        @NotEmpty
         lateinit var scope: String
+
+        @NotEmpty
         lateinit var redirectUri: String
+
+        @NotEmpty
         lateinit var tokenCookieDomain: String
 
         override fun toString(): String {
