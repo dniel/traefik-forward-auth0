381670583cc85393e81e9119de3d673b8f06d49f
Daniel <daniel@engfeldt.net>
2019-02-07 21:19:36 +0100

    add docu

--- src/main/kotlin/dniel/forwardauth/infrastructure/endpoints/AuthorizeEndpoint.kt
+++ src/main/kotlin/dniel/forwardauth/infrastructure/endpoints/AuthorizeEndpoint.kt
@@ -39,12 +39,14 @@ class AuthorizeEndpoint(val authorizeCommandHandler: AuthorizeCommandHandler) {
         val command: AuthorizeCommand = AuthorizeCommand(accessToken, idToken, protocol, host, uri, method)
         val authorizeResult = authorizeCommandHandler.perform(command)
 
+        // if we have an redirect url, it means that we need to authorize
         authorizeResult.redirectUrl?.let {
             LOGGER.debug("Redirect to ${authorizeResult.redirectUrl}")
             val nonceCookie = NewCookie("AUTH_NONCE", authorizeResult.nonce.toString(), "/", authorizeResult.app.tokenCookieDomain, null, -1, false, true);
             return Response.temporaryRedirect(authorizeResult.redirectUrl).cookie(nonceCookie).build()
         }
 
+        // if we have an accesstoken, it means that we have a valid user
         authorizeResult.accessToken?.let {
             val response = Response.ok()
             response.header("Authorization", "Bearer: ${authorizeResult.accessToken}")
