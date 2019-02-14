43af3c767bda36ae4748271f9f671ced7c185cda
Daniel <daniel@engfeldt.net>
2019-02-14 19:32:40 +0100

    remove unused code

--- src/main/kotlin/dniel/forwardauth/AuthApplication.kt
+++ src/main/kotlin/dniel/forwardauth/AuthApplication.kt
@@ -1,28 +1,14 @@
 package dniel.forwardauth
 
-import dniel.forwardauth.infrastructure.endpoints.AuthorizeEndpoint
-import dniel.forwardauth.infrastructure.endpoints.SigninEndpoint
 import dniel.forwardauth.infrastructure.endpoints.AppExceptionMapper
+import dniel.forwardauth.infrastructure.endpoints.AuthorizeEndpoint
 import dniel.forwardauth.infrastructure.endpoints.ServerFaultExceptionMapper
+import dniel.forwardauth.infrastructure.endpoints.SigninEndpoint
 import org.glassfish.jersey.server.ResourceConfig
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.env.Environment
-import org.springframework.core.env.EnumerablePropertySource
-import java.util.LinkedList
-import com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER
-import java.util.stream.StreamSupport
-import org.springframework.core.env.AbstractEnvironment
-import org.springframework.core.env.MutablePropertySources
-
-
-
-
-
-
 
 
 @SpringBootApplication
@@ -43,8 +29,4 @@ class AuthApplication(val auth: AuthProperties) : ResourceConfig() {
 
 fun main(args: Array<String>) {
     val applicationContext = runApplication<AuthApplication>(*args)
-    val environment = applicationContext.environment
-    println(environment.propertySources.forEach({
-        println(it)
-    }))
 }
