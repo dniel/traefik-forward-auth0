92e0addc69b1231395eeb2a245a9189ad6ff95c5
Daniel <daniel@engfeldt.net>
2019-04-30 22:26:00 +0200

    #73 Add some log messages.

--- .travis/deploy_feature.sh
+++ .travis/deploy_feature.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "Push new docker image for feature $BRANCH to DockerHub."
+echo "Push new docker image for FEATURE $BRANCH to DockerHub."
 echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin
+
+echo "Create Tags for Image: $TRAVIS_TAG and $BRANCH"
 docker tag dniel/forwardauth dniel/forwardauth:$TRAVIS_TAG
 docker tag dniel/forwardauth dniel/forwardauth:$BRANCH
 
-# docker push dniel/forwardauth:$TRAVIS_TAG
-# docker push dniel/forwardauth:$BRANCH
+echo "Push tags $TRAVIS_TAG and $BRANCH to DockerHub."
+docker push dniel/forwardauth:$TRAVIS_TAG
+docker push dniel/forwardauth:$BRANCH
+echo "DONE"
--- .travis/deploy_release.sh
+++ .travis/deploy_release.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "Push new docker image for RELEASE $BRANCH to DockerHub."
 echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin
+
+echo "Create Tags for Image: $TRAVIS_TAG, $BRANCH and latest"
 docker tag dniel/forwardauth dniel/forwardauth:$TRAVIS_TAG
 docker tag dniel/forwardauth dniel/forwardauth:$BRANCH
 docker tag dniel/forwardauth dniel/forwardauth:latest
 
+echo "Push tags $TRAVIS_TAG, $BRANCH and latest to DockerHub."
 # docker push dniel/forwardauth:$TRAVIS_TAG
 # docker push dniel/forwardauth:$BRANCH
 # docker push dniel/forwardauth:latest
+echo "DONE"
