13f8477cc97eaa913d3d39095d7f6e0e6d8fead5
Daniel <daniel@engfeldt.net>
2019-04-30 22:18:31 +0200

    #73 Add some log messages.

--- .travis/deploy_feature.sh
+++ .travis/deploy_feature.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Push new docker image for feature $BRANCH to DockerHub."
 echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin
 docker tag dniel/forwardauth dniel/forwardauth:$TRAVIS_TAG
 docker tag dniel/forwardauth dniel/forwardauth:$BRANCH
 
-# docker push dniel/forwardauth
+# docker push dniel/forwardauth:$TRAVIS_TAG
+# docker push dniel/forwardauth:$BRANCH
--- .travis/deploy_release.sh
+++ .travis/deploy_release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Push new docker image for RELEASE $BRANCH to DockerHub."
 echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin
 docker tag dniel/forwardauth dniel/forwardauth:$TRAVIS_TAG
 docker tag dniel/forwardauth dniel/forwardauth:$BRANCH
 docker tag dniel/forwardauth dniel/forwardauth:latest
 
-# docker push dniel/forwardauth
+# docker push dniel/forwardauth:$TRAVIS_TAG
+# docker push dniel/forwardauth:$BRANCH
+# docker push dniel/forwardauth:latest
