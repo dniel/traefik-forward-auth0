0b941a4064fd35a182593988103decfcc99d4c42
Daniel <daniel@engfeldt.net>
2019-05-15 21:28:45 +0200

    #73 tag image with commit time - commit hash and branchname Tag the image so that its easy for humans to understand from where the code actually has been built.

--- .travis/deploy_feature.sh
+++ .travis/deploy_feature.sh
@@ -3,10 +3,10 @@ echo "Push new docker image for FEATURE $BRANCH to DockerHub."
 echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin
 
 echo "Create Tags for Image: $TRAVIS_TAG and $BRANCH"
-docker tag dniel/forwardauth dniel/forwardauth:$TRAVIS_TAG
+docker tag dniel/forwardauth dniel/forwardauth:$COMMIT_TIME-$TRAVIS_TAG-$BRANCH
 docker tag dniel/forwardauth dniel/forwardauth:$BRANCH
 
 echo "Push tags $TRAVIS_TAG and $BRANCH to DockerHub."
-docker push dniel/forwardauth:$TRAVIS_TAG
+docker push dniel/forwardauth:$COMMIT_TIME-$TRAVIS_TAG-$BRANCH
 docker push dniel/forwardauth:$BRANCH
 echo "DONE"
