a61a77549eb533d9ed28883582d12a80c8fa914c
Daniel <daniel@engfeldt.net>
2019-09-07 23:37:22 +0200

    Add some documentation notes from openid specification about how to use the Nonce to secure against replay attacks.

--- src/main/kotlin/dniel/forwardauth/domain/Nonce.kt
+++ src/main/kotlin/dniel/forwardauth/domain/Nonce.kt
@@ -1,5 +1,18 @@
 package dniel.forwardauth.domain
 
+/**
+ * The nonce parameter value needs to include per-session state and be unguessable to attackers.
+ *
+ * One method to achieve this for Web Server Clients is to store a cryptographically random value
+ * as an HttpOnly session cookie and use a cryptographic hash of the value as the nonce parameter.
+ * In that case, the nonce in the returned ID Token is compared to the hash of the session cookie to
+ * detect ID Token replay by third parties. A related method applicable to JavaScript Clients is to
+ * store the cryptographically random value in HTML5 local storage and use a cryptographic hash of this value.
+ *
+ * https://openid.net/specs/openid-connect-core-1_0.html#NonceNotes
+ * https://tools.ietf.org/html/draft-ietf-oauth-v2-threatmodel-06#section-4.6.2
+ * https://tools.ietf.org/html/draft-ietf-oauth-v2-threatmodel-06#section-3.6
+ */
 class Nonce(val value: String) {
 
     override fun toString(): String = value
