81071b7bea3f3cc3f45eacde4fe780b27d5a5def
Daniel <daniel@engfeldt.net>
2019-09-08 21:32:41 +0200

    Add more validation of parameters on login request.

--- src/main/kotlin/dniel/forwardauth/infrastructure/spring/SigninController.kt
+++ src/main/kotlin/dniel/forwardauth/infrastructure/spring/SigninController.kt
@@ -39,61 +39,66 @@ class SigninController(val properties: AuthProperties, val auth0Client: Auth0Cli
      */
     @RequestMapping("/signin", method = [RequestMethod.GET])
     fun signin(@RequestHeader headers: MultiValueMap<String, String>,
-               @RequestParam("code", required = false) code: String,
-               @RequestParam("error", required = false) error: String,
-               @RequestParam("error_description", required = false) errorDescription: String,
-               @RequestParam("state", required = false) state: String,
-               @RequestHeader("x-forwarded-host") forwardedHost: String,
-               @CookieValue("AUTH_NONCE") nonce: String,
+               @RequestParam("code", required = false) code: String?,
+               @RequestParam("error", required = false) error: String?,
+               @RequestParam("error_description", required = false) errorDescription: String?,
+               @RequestParam("state", required = false) state: String?,
+               @RequestHeader("x-forwarded-host") forwardedHost: String?,
+               @CookieValue("AUTH_NONCE") nonce: String?,
                response: HttpServletResponse): ResponseEntity<String> {
         printHeaders(headers)
 
         // if error parameter was received something is going on.
         if (error == "unauthorized") {
             LOGGER.info("Unauthorized error from Auth0 on sign in: ${errorDescription}")
-            throw PermissionDeniedException(errorDescription)
-        } else if (error.isNotEmpty()) {
+            throw PermissionDeniedException(errorDescription ?: "no error description.")
+        } else if (!error.isNullOrEmpty()) {
             LOGGER.error("Signing received unknown error from Auth0 on sign in: ${errorDescription}")
-            throw ApplicationErrorException(errorDescription)
+            throw ApplicationErrorException(errorDescription ?: "no error description.")
         }
 
         LOGGER.debug("Sign in with code=$code")
-        val decodedState = State.decode(state)
-        val receivedNonce = decodedState.nonce.value
-        if (receivedNonce != nonce) {
-            LOGGER.error("SignInFailedNonce received=$receivedNonce sent=$nonce")
-            throw ApplicationErrorException("Nonce cookie didnt match the nonce in state.")
+        if (!code.isNullOrBlank() && !state.isNullOrBlank()) {
+            val decodedState = State.decode(state)
+            val receivedNonce = decodedState.nonce.value
+            if (receivedNonce != nonce) {
+                LOGGER.error("SignInFailedNonce received=$receivedNonce sent=$nonce")
+                throw ApplicationErrorException("Nonce cookie didnt match the nonce in state.")
+            }
+
+            val app = properties.findApplicationOrDefault(forwardedHost)
+            val authorizationCodeExchangeResponse = auth0Client.authorizationCodeExchange(code, app.clientId, app.clientSecret, app.redirectUri)
+            val accessToken = authorizationCodeExchangeResponse.get("access_token") as String
+            val expiresIn = authorizationCodeExchangeResponse.get("expires_in") as Int
+            val idToken = authorizationCodeExchangeResponse.get("id_token") as String
+
+            val accessTokenCookie = Cookie("ACCESS_TOKEN", accessToken)
+            val jwtCookie = Cookie("JWT_TOKEN", idToken)
+            val clearNonceCookie = Cookie("AUTH_NONCE", "delete")
+
+            accessTokenCookie.domain = app.tokenCookieDomain
+            accessTokenCookie.maxAge = expiresIn
+            accessTokenCookie.path = "/"
+            accessTokenCookie.isHttpOnly = true
+            jwtCookie.domain = app.tokenCookieDomain
+            jwtCookie.maxAge = expiresIn
+            jwtCookie.path = "/"
+            jwtCookie.isHttpOnly = true
+            clearNonceCookie.maxAge = 0
+            clearNonceCookie.domain = app.tokenCookieDomain
+            clearNonceCookie.path = "/"
+            clearNonceCookie.isHttpOnly = true
+
+            response.addCookie(accessTokenCookie)
+            response.addCookie(jwtCookie)
+            response.addCookie(clearNonceCookie)
+
+            LOGGER.info("SignInSuccessful, redirect to requested originUrl=${decodedState.originUrl}")
+            return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).location(decodedState.originUrl.uri()).build()
+        } else {
+            LOGGER.error("Unknown request, login redirect request from Auth0 had no code, state or error message.")
+            throw ApplicationErrorException()
         }
-
-        val app = properties.findApplicationOrDefault(forwardedHost)
-        val authorizationCodeExchangeResponse = auth0Client.authorizationCodeExchange(code, app.clientId, app.clientSecret, app.redirectUri)
-        val accessToken = authorizationCodeExchangeResponse.get("access_token") as String
-        val expiresIn = authorizationCodeExchangeResponse.get("expires_in") as Int
-        val idToken = authorizationCodeExchangeResponse.get("id_token") as String
-
-        val accessTokenCookie = Cookie("ACCESS_TOKEN", accessToken)
-        val jwtCookie = Cookie("JWT_TOKEN", idToken)
-        val clearNonceCookie = Cookie("AUTH_NONCE", "delete")
-
-        accessTokenCookie.domain = app.tokenCookieDomain
-        accessTokenCookie.maxAge = expiresIn
-        accessTokenCookie.path = "/"
-        accessTokenCookie.isHttpOnly = true
-        jwtCookie.domain = app.tokenCookieDomain
-        jwtCookie.maxAge = expiresIn
-        jwtCookie.path = "/"
-        jwtCookie.isHttpOnly = true
-        clearNonceCookie.maxAge = 0
-        clearNonceCookie.domain = app.tokenCookieDomain
-        clearNonceCookie.path = "/"
-        clearNonceCookie.isHttpOnly = true
-
-        response.addCookie(accessTokenCookie)
-        response.addCookie(jwtCookie)
-        response.addCookie(clearNonceCookie)
-
-        LOGGER.info("SignInSuccessful, redirect to requested originUrl=${decodedState.originUrl}")
-        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).location(decodedState.originUrl.uri()).build()
     }
 
     private fun printHeaders(headers: MultiValueMap<String, String>) {
