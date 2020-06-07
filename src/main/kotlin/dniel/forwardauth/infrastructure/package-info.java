c1f978433dc9c96680d419c9c58bf125d0454674
dniel <daniel@engfeldt.net>
2020-06-07 12:28:35 +0200

    update docu

---
+++ src/main/kotlin/dniel/forwardauth/infrastructure/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * The infrastructure layer.
+ * <p>Aka. Ports and Adapters parts of the Hexagonal Architecture.</p>
+ * <p>
+ * <h4>References</h4>
+ * <li>
+ * <ul>https://web.archive.org/web/20180822100852/http://alistair.cockburn.us/Hexagonal+architecture</ul>
+ * <ul>https://en.wikipedia.org/wiki/Hexagonal_architecture_(software)</ul>
+ * <ul>https://jeffreypalermo.com/2008/07/the-onion-architecture-part-1/</ul>
+ * </li>
+ * </p>
+ */
+package dniel.forwardauth.infrastructure;
