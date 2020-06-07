e1d484fecc70cc80a4e017553c35bec996a1b52f
dniel <daniel@engfeldt.net>
2020-06-07 21:25:19 +0200

    #151 change response status from 403 to 401 when unauthorized

--- src/main/kotlin/dniel/forwardauth/infrastructure/spring/exceptions/PermissionDeniedException.kt
+++ src/main/kotlin/dniel/forwardauth/infrastructure/spring/exceptions/PermissionDeniedException.kt
@@ -4,7 +4,7 @@ import dniel.forwardauth.application.commandhandlers.AuthorizeHandler
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN)
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 class PermissionDeniedException : ApplicationException {
     constructor(error: AuthorizeHandler.AuthorizeEvent.AccessDenied) : super(error.reason)
 }
