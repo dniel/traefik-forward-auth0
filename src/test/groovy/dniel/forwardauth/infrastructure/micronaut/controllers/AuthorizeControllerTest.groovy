7bb7e36214c21d6e4f9308e787d4806c5245403b
Daniel <daniel@engfeldt.net>
2022-01-01 23:13:16 +0100

    Add AuthorizeControllerTest

---
+++ src/test/groovy/dniel/forwardauth/infrastructure/micronaut/controllers/AuthorizeControllerTest.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dniel.forwardauth.infrastructure.micronaut.controllers
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+
+import static org.hamcrest.Matchers.equalTo
+import static spock.util.matcher.HamcrestSupport.that
+
+@MicronautTest
+class AuthorizeControllerTest extends Specification {
+    def "should test the authenticator"() {
+        given: "some input"
+
+        when: "we do something with the authenticator"
+        def hello = "world"
+
+        then: "we should verify the outcome"
+        that(hello, equalTo("world"))
+    }
+}
